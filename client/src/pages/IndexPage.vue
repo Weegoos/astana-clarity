737fabd9d0ea61d3106ff3f3b579144d564d6bd3
Weegoos <batyr.ashim05@mail.ru>
2025-05-08 17:47:18 +0500

    Client: The Tailwind has been integrated to the project

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -4,12 +4,13 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
+    <div class="bg-red-500">Batyr</div>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
